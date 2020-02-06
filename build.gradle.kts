f32c488e8e8b88d51b54a5c6cbae9231e434f8cc
Deborah Yambenu <deboryambenu@gmail.com>
2020-02-06 15:21:03 +0200

    Migration from mysql to hsqldb

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("org.hsqldb:hsqldb:2.4.0")
     providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
